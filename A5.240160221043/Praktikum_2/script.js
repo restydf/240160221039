3d4f3a2f18110f8ce4b505e14d634522a6f0549d
Salwa_annd <salwa27072006@gmail.com>
2026-05-08 20:46:27 +0700

    JS

--- A5.240160221043/Praktikum_2/script.js
+++ A5.240160221043/Praktikum_2/script.js
@@ -13,3 +13,15 @@
        window.onload = function() {
            changeBackgroundColor();
        };
+
+// Menampilkan pesan sambutan
+        function showWelcomeMessage() {
+            alert("Selamat datang di portofolio saya! Saya adalah seorang pengembang web yang penuh semangat.");
+        }
+
+        window.onload = function() {
+            changeBackgroundColor();
+            showWelcomeMessage(); // Panggil fungsi pesan sambutan
+            displayProjects();
+            displayExperience();
+        };
